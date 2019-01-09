6687e25c95d8c1fdc9a93217553f59772ec389fc
Rubin Peci <rubinpeci@gmail.com>
2019-01-09 11:48:00 -0500

    Update crypto.py and add more functionality

--- util/crypto.py
+++ util/crypto.py
@@ -10,28 +10,77 @@ API_KEY = api_dict['nomics']
 #everything works as intended, unless otherwise commented!
 
 def coins():
+    '''Returns a dictionary of all the currencies available on Nomics'''
     url = API_LINK + 'currencies?key=' + API_KEY
     response = urllib.request.urlopen(url)
     return json.loads(response.read())
 
 def prices():
+    '''Returns the prices for all the currencies on Nomics'''
     url = API_LINK + 'prices?key=' + API_KEY
     response = urllib.request.urlopen(url)
     return json.loads(response.read())
 
 def prices_sparkline(start, end):
+    '''Returns list of sparkline data related to all the currencies on Nomics'''
     url = API_LINK + 'currencies/sparkline?key=' + API_KEY + "&start=" + start + "T00%3A00%3A00Z&end=" + end + "T00%3A00%3A00Z"
     response = urllib.request.urlopen(url)
     return json.loads(response.read())
 
+def candlestick(interval, currency, start = None, end = None):
+    '''Returns candlestick data regarding a certain currency, priced in USD '''
+    if start == None and end == None:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&currency=' + currency
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+    elif start == None:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&currency=' + currency + "&end=" + end + "T00%3A00%3A00Z"
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+    elif end == None:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&currency=' + currency + "&start=" + start + "T00%3A00%3A00Z"
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+    else:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&currency=' + currency + "&start=" + start + "T00%3A00%3A00Z&end=" + end + "T00%3A00%3A00Z"
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+
+def exchange_candles(interval, exchange, market, start = None, end = None):
+    '''Returns candlestick data related ot exchange rates'''
+    if start == None and end == None:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&exchange=' + exchange + '&market=' + market
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+    elif start == None:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&exchange=' + exchange + '&market=' + market + '&end=' + end + 'T00%3A00%3A00Z'
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+    elif end == None:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&exchange=' + exchange + '&market=' + market + '&start=' + start + 'T00%3A00%3A00Z' 
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+    else:
+        url = API_LINK + 'candles?key=' + API_KEY + '&interval=' + interval + '&exchange=' + exchange + '&market=' + market + '&start=' + start + 'T00%3A00%3A00Z' + '&end=' + end + 'T00%3A00%3A00Z'
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+
 def list_coins():
+    '''Returns a list of coins available on the Nomics API'''
     list_of_coins = []
     raw = coins()
     for dict in raw:
         list_of_coins.append(dict['id'])
     return list_of_coins
 
+    
+
 #testing functions
+#All work as intended, unless otherwise stated
 #print(prices())
-print(list_coins())
+#print(list_coins())
 #print(prices_sparkline("2018-12-01", "2018-12-31"))
+#print(candlestick('1d', 'BTC'))
+#print(exchange_candles('1m', 'binance', 'BTCETH', "2018-12-01"))
+#print(exchange_candles('1m', 'binance', 'BTCETH', None, '2018-12-30'))
+#print(exchange_candles('1m', 'binance', 'BTCETH', '2018-12-01' , '2018-12-30'))
